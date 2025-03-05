f9825a3adffff8ffb8da6e21693c65d15cec2507
Ekaterina A <ek.antipova.an@gmail.com>
2025-03-05 20:21:08 +0500

    KS-1-4 remove U-type variables

--- src/main/kotlin/lesson_1/lesson1_task4.kt
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -21,14 +21,12 @@ fun main() {
         Часть года – 2.0547945205479453E-4
         Апогей орбиты – 327000 */
 
-    println("lesson1 task4\n")
-
     val length: String = "40_868_600_000"
-    val age: UByte = 27u
+    val age: Byte = 27
     val partOfDay: Float = 0.075f
-    val seconds: UShort = 6480u
+    val seconds: Short = 6480
     val partOfYear: Double = 2.0547945205479453E-4
-    val apogee: UInt = 327000u
+    val apogee: Int = 327000
 
     println("Расстояние $length")
     println("Возраст - $age")
