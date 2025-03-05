45c9a93bc62c05ea8994d45ba652e1f897ca8111
Ekaterina A <ek.antipova.an@gmail.com>
2025-03-05 21:14:00 +0500

    KS-1-5 add constant and refactor code

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,5 +1,8 @@
 package lesson_1
 
+
+const val SECONDS_OR_MINUTES_COUNT = 60
+
 fun main() {
     /* Задача 5* к Уроку 1
     Объяви переменную с количеством секунд, которые Гагарин провел в космосе.
@@ -7,21 +10,13 @@ fun main() {
     - Выведи в консоль время, проведенное в космосе в формате [часы:минуты:секунды], например так: 01:30:09;
     - Значения часов, минут и секунд отображать двумя цифрами.*/
 
-    println("lesson1 task5\n")
-
     val allSeconds = 6480
 
-    val allMinutes = allSeconds / 60
-    val seconds = allSeconds % 60
-    val hours = allMinutes / 60
-    val minutes = allMinutes % 60
-
-    //println("Общее кол-во минут - $allMinutes")
-    //println("Секунды - $seconds")
-    //println("Часы - $hours")
-    //println("Минуты - $minutes")
+    val allMinutes = allSeconds / SECONDS_OR_MINUTES_COUNT
+    val seconds = allSeconds % SECONDS_OR_MINUTES_COUNT
+    val hours = allMinutes / SECONDS_OR_MINUTES_COUNT
+    val minutes = allMinutes % SECONDS_OR_MINUTES_COUNT
 
     val timeFormat = "%02d:%02d:%02d"
     println("Время проведенное в космосе - ${String.format(timeFormat, hours, minutes, seconds)}")
 }
-
